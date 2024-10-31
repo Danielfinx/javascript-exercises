7e7a3250eb0ffdf7966155f3e5210cd53e0cb56a
Daniel <daniel.e.san.1999@gmail.com>
2024-10-31 10:14:45 -0400

    Solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(...nums) {
+    nums.sort();
+    let sum = 0;
 
+    if (nums[0] < 0 || 
+        nums[0]%1 != 0 ||
+        nums[1]%1 != 0 ||
+        typeof(nums[0]) != "number" ||
+        typeof(nums[1]) != "number"
+    ) {
+        return 'ERROR'
+    };
+    
+    while (nums[0] <= nums.at(-1)){
+        sum+= nums[0];
+        nums[0]++;
+    }
+    return sum;
 };
 
 // Do not edit below this line

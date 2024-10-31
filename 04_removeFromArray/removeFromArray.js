58b47796279d64285faf09118ebd5e70011ff848
Daniel <daniel.e.san.1999@gmail.com>
2024-10-31 09:43:30 -0400

    Solved exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+    const newArray = []
+    for (item of array) {
+        if (!nums.includes(item)) newArray.push(item)
+    }
+    return newArray;
 };
 
 // Do not edit below this line

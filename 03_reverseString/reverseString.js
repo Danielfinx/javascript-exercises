e71600e2303790d49d64bea22a2fc6f79a7da5cf
Daniel <daniel.e.san.1999@gmail.com>
2024-10-30 21:16:36 -0400

    Solved exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const reversed = [];
+    for (letter of word) {
+        reversed.unshift(letter);
+    }
+    return reversed.join('');
 };
 
 // Do not edit below this line

171d8fb4a9a35373b887e5add89caf460af9315e
Daniel <daniel.e.san.1999@gmail.com>
2024-10-30 21:08:05 -0400

    Solved exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let strResult = '';
+    if (num >= 0) {
+        for (let i = 1; i <= num; i++) {
+            strResult+= str;
+        }
+    } else {
+        strResult = 'ERROR';
+    }
+    return strResult;
 };
 
 // Do not edit below this line

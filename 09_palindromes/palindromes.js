2111945f9e56678055e6c07964ab8d9aff61d672
Daniel <daniel.e.san.1999@gmail.com>
2024-11-02 22:13:30 -0400

    Solved exercise 09
    I probably should've used regex for this but i didn't remember
    how to use them.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = [...word.toLowerCase()];
+    let reversedWord = '';
+    for (let i= word.length - 1; i >= 0; i--) {
+        if (word.at(i) == ' ' || 
+            word.at(i) == '.' || 
+            word.at(i) == ',' || 
+            word.at(i) == '!' ||
+            word.at(i) == '¡')
+            {
+            word.splice(i,1);
+        } else {
+            reversedWord += word.at(i);
+        }
+    }
+    return word.join('') == reversedWord;
 };
+
+console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;

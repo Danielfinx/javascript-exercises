b8f125f804f502ba90548d2d97d7fd9276a5045a
Daniel <daniel.e.san.1999@gmail.com>
2024-11-02 23:42:17 -0400

    Solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+function getAge (death, birth) {
+    if (!death) {
+        death= new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    return oldest = array.reduce((old, current) => {
+        const oldAge = getAge(old.yearOfDeath, old.yearOfBirth);
+        const age = getAge(current.yearOfDeath, current.yearOfBirth);
+        return age > oldAge ? current : old;
+    });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
